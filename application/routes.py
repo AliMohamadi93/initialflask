5b420083786518471e701c6619547b3a1645a386
AliMohamadi93 <alimohamadi1@hotmail.co.uk>
2020-02-21 14:47:20 +0000

    initial commit

---
+++ application/routes.py
@@ -0,0 +1,14 @@
+from application import app
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
+@app.route('/about')
+def about():
+    return render_template('about.html',title='About')
+@app.route('/login')
+def login():
+    return render_template('login.html', title ='Login')
+@app.route('/register')
+def register():
+    return render_template('register.html',title ='Registration')
