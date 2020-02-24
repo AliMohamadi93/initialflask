9d75c37c5782ba5866ccb330303531f50de7fe4e
AliMohamadi93 <alimohamadi1@hotmail.co.uk>
2020-02-24 17:11:01 +0000

    initial commit

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://ali:password@127.0.0.1/flaskapp"
+db = SQLAlchemy(app)
+
 from application import routes
