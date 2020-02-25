57188257afc58acf9f63feeea9279305e1273203
AliMohamadi93 <alimohamadi1@hotmail.co.uk>
2020-02-25 15:23:07 +0000

    initial commit

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://ali:password@127.0.0.1/flaskapp"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://AliMohamadi93@mydatabase10:Vauxhallcorsa10@mydatabase10.mysql.database.azure.com/testing"
 db = SQLAlchemy(app)
+from os import getenv
 
+app.config['SECRET_KEY'] = getenv('MY_SECRET_KEY')
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,9 @@
-from application import app
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html', title='Home')
+from flask import render_template, redirect, url_for
+from application import app, db
+from application.models import Posts
+from application.forms import PostForm
+
+
 @app.route('/about')
 def about():
     return render_template('about.html',title='About')
@@ -12,3 +13,29 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html',title ='Registration')
+@app.route('/')
+@app.route('/home')
+def home():
+    postData = Posts.query.first()
+    return render_template('home.html', title='Home', post=postData)
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
